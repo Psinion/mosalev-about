12cbc0cac41e861f0e1daa588bc93414fc1e4d69
Mosalev Daniel <mosalev.daniel@gmail.com>
2024-09-28 20:32:15 +0700

    Update backend dockefile

--- src/MOSBackend/MOS.WebApi/Program.cs
+++ src/MOSBackend/MOS.WebApi/Program.cs
@@ -1,44 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Reflection;
+using MOS.WebApi.StartupConfiguration;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+namespace MOS.WebApi;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
+    public static void Main(string[] args)
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+        CreateHostBuilder(args).Build().Run();
+    }
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(
+                webBuilder => webBuilder.UseStartup<Startup>()
+            );
+    }
 }
---
+++ src/MOSBackend/MOS.WebApi/StartupConfiguration/Startup.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
+
+namespace MOS.WebApi.StartupConfiguration;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { 
+                Title = "MOSBackend",
+                Version = "v1" 
+            });
+        });
+        
+        /*services.AddDbContext<MainDbContext>(options =>
+            options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"))
+        );*/
+
+        services.AddControllersWithViews()
+            .AddJsonOptions(options =>
+            {
+                options
+                    .JsonSerializerOptions
+                    .ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "MOS");
+                options.RoutePrefix = string.Empty;
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseCors();
+
+        app.UseEndpoints(endpoints =>
+            endpoints.MapControllers()
+        );
+    }
+}
