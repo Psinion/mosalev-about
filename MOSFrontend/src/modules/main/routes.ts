ab1d3e75d5870dde2535b26e827b8b3adfc5bea9
Mosalev <mosalev.daniel@gmail.com>
2025-04-05 14:27:46 +0700

    Fix vue build

--- MOSFrontend/src/modules/main/routes.ts
+++ MOSFrontend/src/modules/main/routes.ts
@@ -2,7 +2,6 @@ import { RouteRecordRaw } from "vue-router";
 import { RouteNames } from "@/router/routeNames.ts";
 import LoginPage from "./pages/LoginPage/LoginPage.vue";
 import IndexPage from "@/modules/main/pages/IndexPage/IndexPage.vue";
-import AboutPage from "@/modules/main/pages/AboutPage/AboutPage.vue";
 
 export const publicMainRoutes: RouteRecordRaw[] = [{
   path: "/login",
