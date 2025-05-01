f43f0e3e5763e8bb6513df885787cecf26b4bf95
Mosalev <mosalev.daniel@gmail.com>
2025-05-01 13:25:20 +0700

    Fix backend build

--- MOSBackend/MOS.Application/Modules/Projects/Commands/UpdateProjectCommand.cs
+++ MOSBackend/MOS.Application/Modules/Projects/Commands/UpdateProjectCommand.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Mvc;
 using MOS.Application.Data;
 using MOS.Application.DTOs.Projects.Responses;
 using MOS.Application.OperationResults;
